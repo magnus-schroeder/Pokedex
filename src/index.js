549913aaed67eb548663bfc0bf136563b88cf0cd
magnus-schroeder <72558831+magnus-schroeder@users.noreply.github.com>
2020-10-28 17:10:44 +0100

    name, id and type search completed

--- src/index.js
+++ src/index.js
@@ -1,39 +1,84 @@
 const Pokedex = require("json-pokemon");
 const rawInput = document.querySelector("#search");
 
-rawInput.addEventListener("input", giveResults);
-
 function giveResults(rawInput) {
   const searchInput = rawInput.target.value.toLowerCase();
-  giveNameResults(searchInput);
+  const allTypes = [
+    "normal",
+    "fire",
+    "water",
+    "electric",
+    "grass",
+    "ice",
+    "fighting",
+    "poison",
+    "ground",
+    "flying",
+    "psychic",
+    "bug",
+    "rock",
+    "ghost",
+    "dragon",
+    "dark",
+    "steel",
+    "fairy"
+  ];
+  if (isNaN(searchInput)) {
+    const inputArray = searchInput.toLowerCase().split(" ");
+    if (allTypes.includes(inputArray[0]) & (inputArray.length <= 2)) {
+      addResultsToWebsite(giveTypeResults(inputArray));
+    } else {
+      addResultsToWebsite(giveNameResults(searchInput));
+    }
+  } else {
+    addResultsToWebsite(giveIdResults(searchInput));
+  }
 }
 
 function giveNameResults(searchInput) {
-  const searchResults = Pokedex.filter((n) => {
+  return Pokedex.filter((n) => {
     return n.name.toLowerCase().includes(searchInput);
   });
-  addResultsToWebsite(searchResults);
-  console.log(searchResults);
+}
+
+function giveTypeResults(inputArray) {
+  const results = filter(Pokedex, inputArray[0]);
+  if (inputArray.length === 2) {
+    return filter(results, inputArray[1]);
+  }
+  return results;
+}
+
+function filter(unfiltered, filter) {
+  return unfiltered.filter((n) => {
+    const types = n.typeList.map((type) => {
+      return type.toLowerCase();
+    });
+    return types.includes(filter);
+  });
+}
+
+function giveIdResults(searchInput) {
+  return Pokedex.filter((n) => {
+    const stringId = n.id + "";
+    return stringId.includes(searchInput);
+  });
 }
 
 function addResultsToWebsite(searchResults) {
+  const objectKeys = ["id", "name", "typeList"];
   clearResults(document.getElementById("results"));
-
   if (searchResults.length !== 809) {
     searchResults.forEach((result) => {
+      const id = "id" + result.id;
       const row = document.createElement("TR");
-      row.setAttribute("id", "row");
-      //    row.innerHTML = result.id;
+      row.setAttribute("id", id);
       document.getElementById("results").appendChild(row);
-      const tdId = document.createElement("TD");
-      tdId.innerText = result.id;
-      document.getElementById("row").appendChild(tdId);
-      const tdName = document.createElement("TD");
-      tdName.innerText = result.name;
-      document.getElementById("row").appendChild(tdName);
-      const tdType = document.createElement("TD");
-      tdType.innerText = result.typeList;
-      document.getElementById("row").appendChild(tdType);
+      objectKeys.forEach((key) => {
+        const td = document.createElement("TD");
+        td.innerText = result[key];
+        document.getElementById(id).appendChild(td);
+      });
     });
   }
 }
@@ -43,3 +88,5 @@ function clearResults(parent) {
     parent.removeChild(parent.lastChild);
   }
 }
+
+rawInput.addEventListener("input", giveResults);
